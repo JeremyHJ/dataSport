ae0365e5cd840c10bc04c57c176a9e9f81b067d5
JeremyHJ <JeremyHJ@email.com>
2019-07-13 02:48:36 +0800

    开发分支

---
+++ src/main/java/com/hjcc/ydItem/dataSport/control/BaseControl.java
@@ -0,0 +1,10 @@
+package com.hjcc.ydItem.dataSport.control;/**
+ Created by 胡杰 on 2019/3/30. */
+
+/**
+ * FileName: com.hjcc.ydItem.dataSport.control.BaseControl.java
+ * Author: Jeremy_Hu
+ * Date: 2019/3/30 下午8:14
+ */
+public class BaseControl {
+}
